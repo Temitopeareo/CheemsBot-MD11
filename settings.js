9591a7f44a40e25c33c9de0218aa8486e198b4d9
Temitopeareo <132304443+Temitopeareo@users.noreply.github.com>
2024-06-22 11:28:31 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,19 +19,19 @@ global.socialm = "GitHub: DGXeon" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //new
-global.botname = 'Cheems Bot MD V11' //ur bot name
-global.ownernumber = '916909137213' //ur owner number
-global.ownername = '🦄드림 가이 Xeon' //ur owner name
-global.websitex = "https://youtu.be/mu5m6aB6P5k"
-global.wagc = "https://whatsapp.com/channel/0029VaG9VfPKWEKk1rxTQD20"
-global.themeemoji = '🪀'
-global.wm = "Xeon Bot Inc."
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD11' //script link
-global.packname = "Sticker By"
-global.author = "🦄드림 가이 Xeon\n\n+916909137213"
+global.botname = 'Abro Tech Bot' //ur bot name
+global.ownernumber = '2348100151048' //ur owner number
+global.ownername = 'Abro Tech' //ur owner name
+global.websitex = "https://tiktok.com/@abro__speed
+global.wagc = "https://whatsapp.com/channel/0029VaidGkA6hENhcPlyuu1e"
+global.themeemoji = '🤖'
+global.wm = "Abro Tech Inc."
+global.botscript = 'https://github.com/Temitopeareo/CheemsBot-MD11' //script link
+global.packname = "Sticker By Abro Tech"
+global.author = "🤖 Abro Tech\n\n+2348100151048"
 global.creator = "916909137213@s.whatsapp.net"
-global.xprefix = '.'
-global.premium = ["916909137213"] // Premium User
+global.xprefix = '!'
+global.premium = ["2348100151048"] // Premium User
 global.hituet = 0
 
 //bot sett
